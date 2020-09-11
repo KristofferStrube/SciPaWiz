c6260513fc6e5e7a0b7c3b7725e2f832f88e3444
Mathias Ravn Tversted <Mathias@thesun.dk>
2020-09-11 14:10:21 +0200

    Før delvist

--- download.py
+++ download.py
@@ -1,14 +1,26 @@
 from scholarly import scholarly
 import numpy as np
 import time
+from fp.fp import FreeProxy
 
 
 scholar = 'Davide Mottin'
 scholar_dir = "files/DavideMottin"
 scholarly.use_tor(tor_sock_port=9050, tor_control_port=9051, tor_pw="scholarly_password")
 
+
 def sleep():
     time.sleep((30-5)*np.random.random()+5)
+
+
+def set_new_proxy():
+    while True:
+        proxy = FreeProxy(rand=True, timeout=1).get()
+        proxy_works = scholarly.use_proxy(http=proxy, https=proxy)
+        if proxy_works:
+            break
+    print("Working proxy:", proxy)
+    return proxy
 
 
 def extract_author_information(author):
@@ -31,10 +43,11 @@ def extract_paper_information(paper):
 
 citingauthors = set()
 
+
 def bypass_rate_limit_pub(pub, f):
     try:
         for citation in pub.citedby:
-            sleep()
+            # sleep()
             f.write(f"{citation.bib['title']}")
             citers = citation.bib['author']
             try:
@@ -43,18 +56,20 @@ def bypass_rate_limit_pub(pub, f):
             except:
                 pass
     except:
-        sleep()
+        set_new_proxy()
+        # sleep()
         bypass_rate_limit_pub(pub, f)
 
 
 def bypass_rate_limit_citing(citingauthor, f):
     try:
-        sleep()
+        # sleep()
         search_query = scholarly.search_author(citingauthor)
         cauthor = next(search_query).fill()
         f.write(f"{extract_author_information(cauthor)}\n")
     except:
-        sleep()
+        # sleep()
+        set_new_proxy()
         bypass_rate_limit_citing(citingauthor, f)
 
 
@@ -67,7 +82,7 @@ with open(f"{scholar_dir}/{scholar}", 'w+') as f:
 
 with open(f"{scholar_dir}/citingpapers", "w+") as f:
     for pub in author.publications:
-        sleep()
+        # sleep()
         # citations_titles = [citation.bib['title'] for citation in pub.citedby]
         bypass_rate_limit_pub(pub, f)
 
