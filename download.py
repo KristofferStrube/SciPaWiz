e0d9862f7c2dc5864fc6d064d4f5d2e2dd0f1f17
Mathias Ravn Tversted <Mathias@thesun.dk>
2020-09-11 12:51:42 +0200

    Random sleep

--- download.py
+++ download.py
@@ -1,9 +1,14 @@
 from scholarly import scholarly
-from time import sleep
+import numpy as np
+import time
 
 
 scholar = 'Davide Mottin'
 scholar_dir = "files/DavideMottin"
+
+
+def sleep():
+    time.sleep((30-5)*np.random.random()+5)
 
 
 def extract_author_information(author):
@@ -26,10 +31,10 @@ def extract_paper_information(paper):
 
 citingauthors = set()
 
-
 def bypass_rate_limit_pub(pub, f):
     try:
         for citation in pub.citedby:
+            sleep()
             f.write(f"{citation.bib['title']}")
             citers = citation.bib['author']
             try:
@@ -38,17 +43,18 @@ def bypass_rate_limit_pub(pub, f):
             except:
                 pass
     except:
-        sleep(5*60)
+        sleep()
         bypass_rate_limit_pub(pub, f)
 
 
 def bypass_rate_limit_citing(citingauthor, f):
     try:
+        sleep()
         search_query = scholarly.search_author(citingauthor)
         cauthor = next(search_query).fill()
         f.write(f"{extract_author_information(cauthor)}\n")
     except:
-        sleep(10*60)
+        sleep()
         bypass_rate_limit_citing(citingauthor, f)
 
 
@@ -61,10 +67,10 @@ with open(f"{scholar_dir}/{scholar}", 'w+') as f:
 
 with open(f"{scholar_dir}/citingpapers", "w+") as f:
     for pub in author.publications:
+        sleep()
         # citations_titles = [citation.bib['title'] for citation in pub.citedby]
-        sleep(15)
         bypass_rate_limit_pub(pub, f)
-            
+
 
 with open(f"{scholar_dir}/citingauthors", 'w+') as f:
     for citingauthor in citingauthors:
