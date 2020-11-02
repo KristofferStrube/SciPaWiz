c283ddd553d96301865bc43ea6220af741a7f213
KristofferStrube <i@kristoffer-strube.dk>
2020-11-02 17:33:17 +0100

    Count represents number of nodes using Set.

--- visualizer/loadData.js
+++ visualizer/loadData.js
@@ -3,6 +3,13 @@ var proxy = "" //"https://cors-anywhere.herokuapp.com/";
 var count;
 var total;
 
+var localStorage = window.localStorage;
+
+var cachedInfo = JSON.parse(localStorage.getItem('cachedInfo'));
+if (cachedInfo == null) {
+    cachedInfo = {}
+}
+
 Array.prototype.uniqueText = function() {
     var arr = [];
     for (var i = 0; i < this.length; i++) {
@@ -37,6 +44,14 @@ function GetPapersFromAuthor(author, selectElement) {
     xhttp.send();
 }
 
+function union(setA, setB) {
+    let _union = new Set(setA)
+    for (let elem of setB) {
+        _union.add(elem)
+    }
+    return _union
+}
+
 function LoadPaper(doi, layers, infoCallback, citersCallback, updateGraphCallback) {
     if (layers != 0) {
         var citersInfo = new XMLHttpRequest();
@@ -46,30 +61,41 @@ function LoadPaper(doi, layers, infoCallback, citersCallback, updateGraphCallbac
                 var respons = JSON.parse(citersInfo.responseText)
                 var citers = respons.map(p => p.citing.substring(8, p.citing.length));
                 citersCallback(doi, citers)
-                total += citers.length
-                document.getElementById('total').innerHTML = total;
-                citers.forEach(c => LoadPaper(c, layers - 1, infoCallback, citersCallback, updateGraphCallback))
+                if (layers - 1 != 0) {
+                    total = union(total, citers);
+                    document.getElementById('total').innerHTML = total.size;
+                    citers.forEach(c => LoadPaper(c, layers - 1, infoCallback, citersCallback, updateGraphCallback))
+                }
             }
         };
         citersInfo.open("GET", proxy + `https://opencitations.net/index/api/v1/citations/${doi}`, true);
         citersInfo.send();
     }
 
-    var paperInfo = new XMLHttpRequest();
-    paperInfo.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            var respons = JSON.parse(paperInfo.responseText);
+    if (!count.has(doi)) {
+        if (cachedInfo[doi] != undefined) {
+            returnInfo(cachedInfo[doi])
+        } else {
+            var paperInfo = new XMLHttpRequest();
+            paperInfo.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    // Typical action to be performed when the document is ready:
+                    var respons = JSON.parse(paperInfo.responseText);
+                    cachedInfo[doi] = respons;
+                    localStorage.setItem('cachedInfo', JSON.stringify(cachedInfo));
+                    returnInfo(respons);
+                }
+            };
+            paperInfo.open("GET", proxy + `https://opencitations.net/index/api/v1/metadata/${doi}`, true);
+            paperInfo.send();
+        }
+
+        function returnInfo(respons) {
             if (!Array.isArray(respons)) return;
             if (respons.length == 0) return;
             infoCallback(doi, respons[0].title)
-            count++;
-            document.getElementById('count').innerHTML = count;
-            if (count == total) {
-                updateGraphCallback();
-            }
+            count.add(doi)
+            document.getElementById('count').innerHTML = count.size;
         }
-    };
-    paperInfo.open("GET", proxy + `https://opencitations.net/index/api/v1/metadata/${doi}`, true);
-    paperInfo.send();
+    }
 }
