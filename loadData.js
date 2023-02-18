67bd24826517edf0908a2a9f17da018c783b4fc1
Kristoffer Strube <kristoffer.strube@gmail.com>
2023-02-18 16:37:48 +0100

    Update loadData.js

--- loadData.js
+++ loadData.js
@@ -127,7 +127,7 @@ function LoadPaper(doi, layers, infoCallback, citersCallback) {
         function returnInfo(respons) {
             if (!Array.isArray(respons)) return;
             if (respons.length == 0) return;
-            infoCallback(doi, respons[0].title, respons[0].source_title, respons[0].author, respons[0].year)
+            infoCallback(doi, respons[0].title, respons[0].publisher.replaceAll(/ \[.*?\]/g,''), respons[0].author.replaceAll(/ \[.*?\]/g,''), respons[0].pub_date.split('-')[0])
             count.add(doi)
             document.getElementById('count').innerHTML = count.size;
         }
